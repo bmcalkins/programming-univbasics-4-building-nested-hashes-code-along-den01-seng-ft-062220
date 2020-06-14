557619b4d79e3bc4736306f607178cad32c61241
Bradley Calkins <b.raders1013@gmail.com>
2020-06-14 01:04:17 +0000

    Automatically backed up by Learn

--- lib/03_third_challenge.rb
+++ lib/03_third_challenge.rb
@@ -9,12 +9,8 @@ def third_challenge
    :capulet => {
       :patriarch => { "Lord Capulet", age: "50"},
       :matriarch => { "Lady Capulet", age: "51"},
-      :heroine => {
-        #your key/value pairs here
-        },
+      :heroine => {name: "Juliet", age: "15", status: "alive"},
       :heroine_friends => []
    }
   }
-
-  
 end
